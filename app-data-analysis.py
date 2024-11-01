eba7974ca37a1fe5e7018132d0acc274b582873f
jessicat8 <jessicatong14@gmail.com>
2024-11-01 13:09:38 -0700

    initial commit

---
+++ app-data-analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+
+
+# read in json files
+# convert to dictionaries, store in a list
+# analyze with pandas
+
+""" 
+Questions
+1. Top categories of apps (4)
+2. Top types of purposes (overall, by category) (4)
+3. Type of data (overall, by category) (4-10)
+"""
+
