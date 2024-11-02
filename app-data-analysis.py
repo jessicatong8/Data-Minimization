a1e9d27b76e2ce51dc0aee3108e7703688a19736
jessicat8 <jessicatong14@gmail.com>
2024-11-02 16:26:44 -0700

    comments

--- app-data-analysis.py
+++ app-data-analysis.py
@@ -2,17 +2,6 @@ import json
 import os
 import pandas as pd
 
-# read in json files
-# convert to dictionaries, store in a list
-# analyze with pandas
-
-""" 
-Questions
-1. Top categories of apps (4)
-2. Top types of purposes (overall, by category) (4)
-3. Type of data (overall, by category) (4-10)
-"""
-
 
 def get_files(directory):
     """
