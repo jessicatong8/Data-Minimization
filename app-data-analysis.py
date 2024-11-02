c56c20107168f14ba50c6529ae6bdab8184183cb
jessicat8 <jessicatong14@gmail.com>
2024-11-02 14:49:04 -0700

    read in json files

--- app-data-analysis.py
+++ app-data-analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import os
 
 
 # read in json files
@@ -13,3 +14,39 @@ Questions
 3. Type of data (overall, by category) (4-10)
 """
 
+
+def getFiles(directory):
+    # assign directory
+
+    files_list = []
+    
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        file = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(file):
+            files_list.append(file)
+
+    return files_list
+
+def parseFiles(files_list):
+    data_list = []
+
+    for jsonfile in files_list:
+        with open(jsonfile, 'r') as file:
+            data = json.load(file)
+            data_list.append(data)
+
+    return data_list
+    
+
+def main():
+    files_list = getFiles("json_files_ios")
+    print(len(files_list))
+    data_list = parseFiles(files_list)
+    print(len(data_list))
+
+
+if __name__ == "__main__":
+    main()
