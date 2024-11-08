77f43ef4df98a316071ce883af5d977018b92942
jessicat8 <jessicatong14@gmail.com>
2024-11-07 22:38:34 -0800

    tried data types with df

--- data_types.py
+++ data_types.py
@@ -1,56 +1,86 @@
-from apps import load_json, dump_json
+from apps import *
 import pandas as pd
 
 
+def data_types(in_files, categories, out_file):
+    """
+    :param in_files: (list) list of filenames (strings) for json files with app info
+    :param categories: (list) list of category names (strings) corresponding to files
+    :return: (df) dataframe containing number of apps collecting data for each purpose by category
+    """
+    # list to store dataframes for each category
+    dataframes = []
 
-def data_types(json_file, out_filename):
-    # load json file to list of apps
-    app_list = load_json(json_file)
+    # iterate over all categories
+    for i in range(len(in_files)):
+        #print(i)
+        # load json file to list of apps
+        app_list = load_json(in_files[i])
 
-    #print(type(data))
-    data_types_dict = {}
+        # dictionary to store number of apps collecting data for each purpose
+        data_type_dict = {}
 
-    for app in app_list:
-        for data_type in app["data_linked"].values():
+        # iterate over all apps in the current category and count the number of apps collecting data for each purpose
+        for app in app_list:
+            #print(i)
+            for purpose in app["data_linked"].keys():
+                print("Purpose:", purpose)
+                for broad_data_type in app["data_linked"].values():
+                    #print("Broad Data Type: ", broad_data_type)
+                    #print(type(broad_data_type.values()))
+                    for key in broad_data_type.keys():
+                        for specific_data_type in broad_data_type[key]:
+                            #print(specific_data_type)
+                            if specific_data_type not in data_type_dict:
+                                data_type_dict[specific_data_type] = 1
+                            else:
+                                data_type_dict[specific_data_type] += 1
+                #print(i)
+                print(data_type_dict)
+                print(df.loc[0,purpose])
+                
+                df.loc[0, purpose] = data_type_dict
+                print(df)
+                break
+
+            break
         
-            if data_type not in data_types_dict:
-                data_types_dict[data_type] = 1
+            # if app["data_track"]:
+            #     if "Tracking" not in purposes_dict:
+            #         purposes_dict["Tracking"] = 1
+                
+            #     else:
+            #         purposes_dict["Tracking"] += 1
 
-            else:
-                data_types_dict[data_type] += 1
-        
-        for data_type in app["data_track"].values():
-            if data_type not in data_types_dict:
-                data_types_dict[data_type] = 1
-            
-            else:
-                data_types_dict[data_type] += 1
+        # sorts dictionary by value (increasing rating)
+        # Source: https://realpython.com/sort-python-dictionary/
+        #data_type_dict = dict(sorted(purposes_dict.items(), key=lambda item: item[1], reverse=True))
 
-    # sorts dictionary by value (increasing rating)
-    # Source: https://realpython.com/sort-python-dictionary/
-    data_types_dict = dict(sorted(data_types_dict.items(), key=lambda item: item[1], reverse=True))
+        # # create a dataframe from dictionary for each category
+        # df = pd.DataFrame(purposes_dict, index=[0])
+        # # add column for category name
+        # df.insert(0, 'Category', categories[i])
+        # # add dataframe to list of dataframes for all categories
+        # dataframes.append(df)
+   
+    # # combine all dataframes
+    # purposes_by_category = pd.concat(dataframes)
 
-    # write sorted dictionary to a new json file
-    dump_json(data_types_dict, out_filename)
+    # # save dataframe to csv file
+    # purposes_by_category.to_csv(out_file, index = False)
 
-    df = pd.DataFrame.from_dict(data_types_dict, index=[0])
-    print(df)
-    return data_types_dict
+    # return purposes_by_category
 
 
-#get list of purposes for all ios game apps
-print(data_types("ios_games.json","ios_games_purposes.json"))
-#get list of purposes for all ios heath & fitness apps
-print(data_types("ios_health.json","ios_health_purposes.json"))
-#get list of purposes for all ios lifestyle apps
-print(data_types("ios_lifestyle.json","ios_lifestyle_purposes.json"))
-#get list of purposes for all ios shopping apps
-print(data_types("ios_shopping.json","ios_shopping_purposes.json"))
-#get list of purposes for all ios travel apps
-print(data_types("ios_travel.json","ios_travel_purposes.json"))
-#get list of purposes for all ios app types
-print(data_types("ios_apps.json","ios_purposes.json"))
+df = pd.read_csv('ios_data_types.csv')
 
 
 
+in_files = ["ios_apps.json", "ios_games.json", "ios_lifestyle.json", "ios_shopping.json", "ios_travel.json", "ios_health.json"]
+top_categories = ["All Apps", 'Games', 'Lifestyle', 'Shopping', 'Travel', 'Health & Fitness']
+data_types(in_files, top_categories, "ios_data_types.csv")
 
+
+# in_files = ["ios_apps.json"]
+# top_categories = ["All Apps", 'Games', 'Lifestyle', 'Shopping', 'Travel', 'Health & Fitness']
+# data_types(in_files, top_categories, "ios_data_types.csv")
