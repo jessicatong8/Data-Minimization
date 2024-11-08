e3f7ba951e2577bdf1ada6315ca7efee816b6110
jessicat8 <jessicatong14@gmail.com>
2024-11-07 23:07:28 -0800

    maybe it works

--- data_types.py
+++ data_types.py
@@ -2,48 +2,44 @@ from apps import *
 import pandas as pd
 
 
-def data_types(in_files, categories, out_file):
+def data_types(in_file, out_file):
     """
     :param in_files: (list) list of filenames (strings) for json files with app info
     :param categories: (list) list of category names (strings) corresponding to files
     :return: (df) dataframe containing number of apps collecting data for each purpose by category
     """
-    # list to store dataframes for each category
-    dataframes = []
 
-    # iterate over all categories
-    for i in range(len(in_files)):
-        #print(i)
-        # load json file to list of apps
-        app_list = load_json(in_files[i])
+    # load json file to list of apps
+    app_list = load_json(in_file)
 
-        # dictionary to store number of apps collecting data for each purpose
-        data_type_dict = {}
+    # dictionary to store number of apps collecting data for each purpose
+    dict1 = {}
 
-        # iterate over all apps in the current category and count the number of apps collecting data for each purpose
-        for app in app_list:
-            #print(i)
-            for purpose in app["data_linked"].keys():
-                print("Purpose:", purpose)
-                for broad_data_type in app["data_linked"].values():
-                    #print("Broad Data Type: ", broad_data_type)
-                    #print(type(broad_data_type.values()))
-                    for key in broad_data_type.keys():
-                        for specific_data_type in broad_data_type[key]:
-                            #print(specific_data_type)
-                            if specific_data_type not in data_type_dict:
-                                data_type_dict[specific_data_type] = 1
-                            else:
-                                data_type_dict[specific_data_type] += 1
-                #print(i)
-                print(data_type_dict)
-                print(df.loc[0,purpose])
-                
-                df.loc[0, purpose] = data_type_dict
-                print(df)
-                break
+    # iterate over all apps in the current category and count the number of apps collecting data for each purpose
+    for app in app_list:
+        for purpose in app["data_linked"].keys():
+            dict2 = {}
+            #print("Purpose:", purpose)
+            for broad_data_type in app["data_linked"].values():
+                #print("Broad Data Type: ", broad_data_type)
+                #print(type(broad_data_type.values()))
+                for key in broad_data_type.keys():
+                    for specific_data_type in broad_data_type[key]:
+                        #print(specific_data_type)
+                        if specific_data_type not in dict2:
+                            dict2[specific_data_type] = 1
+                        else:
+                            dict2[specific_data_type] += 1
+       
+            for key in dict2:
+                if key in dict1:
+                    dict1[key] = dict1[key] + dict2[key]
+                else:
+                    dict1[key] = dict2[key]
+    
+    print(dict1)
 
-            break
+        #break
         
             # if app["data_track"]:
             #     if "Tracking" not in purposes_dict:
@@ -76,11 +72,10 @@ df = pd.read_csv('ios_data_types.csv')
 
 
 
-in_files = ["ios_apps.json", "ios_games.json", "ios_lifestyle.json", "ios_shopping.json", "ios_travel.json", "ios_health.json"]
-top_categories = ["All Apps", 'Games', 'Lifestyle', 'Shopping', 'Travel', 'Health & Fitness']
-data_types(in_files, top_categories, "ios_data_types.csv")
-
-
-# in_files = ["ios_apps.json"]
+# in_files = ["ios_apps.json", "ios_games.json", "ios_lifestyle.json", "ios_shopping.json", "ios_travel.json", "ios_health.json"]
 # top_categories = ["All Apps", 'Games', 'Lifestyle', 'Shopping', 'Travel', 'Health & Fitness']
 # data_types(in_files, top_categories, "ios_data_types.csv")
+
+
+in_file = "ios_apps.json"
+data_types(in_file, "test.csv")
