2704f2fa407174a7bc4a4bf8e05a171502824cbb
jessicat8 <jessicatong14@gmail.com>
2024-11-12 21:10:26 -0800

    android apps by rating

--- apps.py
+++ apps.py
@@ -51,8 +51,10 @@ def parse_files(files_list, out_filename):
     data_list = []
     # iterate over files
     for jsonfile in files_list:
+        if jsonfile == "json_files_android/.DS_Store":
+            continue
         # load json file to dictionary
-        data = json.load(jsonfile)
+        data = load_json(jsonfile)
         # add to list 
         data_list.append(data)
 
@@ -62,9 +64,10 @@ def parse_files(files_list, out_filename):
     return data_list
 
 
-def categories(json_file, out_filename):
+def categories(appstore, json_file, out_filename):
     """
     counts apps by category
+    :param appstore: (string): "ios" or "android"
     :param json_file: (string): Name of json file containing app data.
     :param out_filename: (string): Name of the output json file.
     :return: (dict): Dictionary where keys are categories and values are the number of apps in that category.
@@ -75,13 +78,33 @@ def categories(json_file, out_filename):
     #print(type(data))
     category_dict = {}
 
+    count = 0
     for app in data:
-        category = app["app_info"]["App Category"]
-
+        #print(app["App name"])
+        if appstore == "ios":
+            category = app["app_info"]["App Category"]
+            numReviews = app["app_info"]["No. of Ratings"]
+        elif appstore == "android":
+            if len(app["App category"]) < 1:
+                count += 1
+            elif len(app["App category"]) > 1:
+                category = "Games"
+            else:
+                category = app["App category"][0]
+            numReviews = app["Total reviews"]
+            print(numReviews)
+            if numReviews == "No reviews":
+                numReviews = 0
+            else:
+                numReviews = int(numReviews)
+            print(numReviews)
+    
         if category not in category_dict:
-            category_dict[category] = 1
+            category_dict[category] = numReviews
         else:
-            category_dict[category] += 1
+            category_dict[category] += numReviews
+            
+    print("not counted: ", count)
 
     # sorts dictionary by value (increasing rating)
     # Source: https://realpython.com/sort-python-dictionary/
@@ -91,6 +114,39 @@ def categories(json_file, out_filename):
     dump_json(category_dict, out_filename)
     
     return category_dict
+
+def top_apps(appstore, json_file, out_filename):
+    # load json file to dictionary
+    data = load_json(json_file)
+
+    #print(type(data))
+    name_dict = {}
+
+    for app in data:
+        if appstore == "ios":
+            name = app["app_info"]["App Name"]
+            numReviews = app["app_info"]["No. of Ratings"]
+        elif appstore == "android":
+            name = app["App name"]
+            numReviews = app["Total reviews"]
+            if numReviews == "No reviews":
+                numReviews = 0
+            else:
+                numReviews = int(numReviews)
+
+        if name not in name_dict:
+            name_dict[name] = numReviews
+        else:
+            name_dict[name] += numReviews
+
+    # sorts dictionary by value (increasing rating)
+    # Source: https://realpython.com/sort-python-dictionary/
+    name_dict = dict(sorted(name_dict.items(), key=lambda item: item[1], reverse=True))
+
+    # write sorted dictionary to a new json file
+    dump_json(name_dict, out_filename)
+    
+    return name_dict
 
 def top_categories(categories, num):
     """
@@ -126,7 +182,6 @@ def group_by_category(json_file, category, out_filename):
         
     return category_sorted
 
-
     
 def main():
     # #place ios json files into list of files
@@ -146,23 +201,21 @@ def main():
     # data_list = parse_files(files_list, "android_apps.json") 
     # #check the length is accurate and every dictionary has been added
     # print(len(data_list))
-
-    """
-    Android files that don't work
-    json_files_android/com.fireletter.text.photo.alphabet.alphabet.fireletter.json
-    json_files_android/com.molatra.trainchinese.json
-    json_files_android/com.jaffajam.santasslamdunk.json
-
-    """
     
-    top_categories = ['Games', 'Lifestyle', 'Shopping', 'Travel', 'Health & Fitness']
-    out_files = ["ios_games.json", "ios_lifestyle.json", "ios_shopping.json", "ios_travel.json", "ios_health.json"]
     
-    # group apps by category into json files
-    for i in range(len(top_categories)):
-        grouped = group_by_category("ios_apps.json", top_categories[i], out_files[i])
-        # check appropriate number of apps have been grouped together
-        print(len(grouped))
+    #categories("ios", "ios_apps.json", "ios_categories_ratings.json")
+    #top_apps("ios_apps.json", "ios_top_apps_ratings.json")
+    #categories("android", "android_apps.json", "android_categories_ratings.json")
+    top_apps("android", "android_apps.json", "android_top_apps_ratings.json")
+    
+    # top_categories = ['Games', 'Lifestyle', 'Shopping', 'Travel', 'Health & Fitness']
+    # out_files = ["ios_games.json", "ios_lifestyle.json", "ios_shopping.json", "ios_travel.json", "ios_health.json"]
+
+    # # group apps by category into json files
+    # for i in range(len(top_categories)):
+    #     grouped = group_by_category("ios_apps.json", top_categories[i], out_files[i])
+    #     # check appropriate number of apps have been grouped together
+    #     print(len(grouped))
 
 if __name__ == "__main__":
     main()
